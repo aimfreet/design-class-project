f6d875c06900d290cd63ffc20172032ddc32d244
Aimee Maroney <ammarone@syr.edu>
2021-05-10 21:05:12 -0400

    generate

---
+++ docs/_nuxt/0d9418e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{254:function(e,n,t){"use strict";t.r(n);var l=t(20),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("Test")])}),[],!1,null,null,null);n.default=component.exports}}]);
